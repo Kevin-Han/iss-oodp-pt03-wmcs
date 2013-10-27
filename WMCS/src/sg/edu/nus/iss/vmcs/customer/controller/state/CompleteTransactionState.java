38079e73bc5014d87703129e42461a7e2ceae386
xiong.gaogao@gmail.com <xiong.gaogao@gmail.com@153f8544-f85d-40b6-333d-ce60a2be1993>
2013-10-27 15:30:30 +0000

    state pattern done

--- WMCS/src/sg/edu/nus/iss/vmcs/customer/controller/state/CompleteTransactionState.java
+++ WMCS/src/sg/edu/nus/iss/vmcs/customer/controller/state/CompleteTransactionState.java
@@ -1,5 +1,66 @@
 package sg.edu.nus.iss.vmcs.customer.controller.state;
 
+import sg.edu.nus.iss.vmcs.customer.controller.ChangeGiver;
+import sg.edu.nus.iss.vmcs.customer.controller.CoinReceiver;
+import sg.edu.nus.iss.vmcs.customer.controller.DispenseController;
+import sg.edu.nus.iss.vmcs.customer.controller.TransactionController;
+
 public class CompleteTransactionState implements TransactionState {
+
+	private TransactionController controller;
+	
+	private DispenseController dispenseController;
+	private CoinReceiver coinReceiver;
+	private ChangeGiver changeGiver;
+	
+	public CompleteTransactionState(TransactionController controller) {
+		this.controller = controller;
+		dispenseController = controller.getDispenseController();
+		coinReceiver = controller.getCoinReceiver();
+		changeGiver = controller.getChangeGiver();
+	}
+	
+	@Override
+	public void startTransaction(int drinkIndex) {
+		System.out.println("Invalid operation..");
+	}
+
+	@Override
+	public void processMoneyReceived(int total) {
+		System.out.println("Invalid operation..");
+	}
+
+	@Override
+	public void completeTransaction() {
+		System.out.println("Completing transaction...");
+		boolean drinkDispensed = dispenseController.dispenseDrink(controller.getSelectedDrinkIndex());
+		controller.setDrinkDispensed(drinkDispensed);
+		controller.setSelectedDrinkIndex(-1);
+		
+		int change = coinReceiver.getTotalCash() - controller.getSelectedDrinkPrice();
+		boolean changeGiven = changeGiver.giveChange(change);
+		controller.setChangeGiven(changeGiven);
+		
+		coinReceiver.storeCash();
+		dispenseController.allowSelection(true);
+		
+		// state transition
+		controller.setTransactionState(controller.getSelectDrinkState());
+	}
+
+	@Override
+	public void cancelTransaction() {
+		System.out.println("Invalid operation..");
+	}
+
+	@Override
+	public void terminateFault() {
+		
+	}
+
+	@Override
+	public void terminateTransaction() {
+		
+	}
 
 }
--- WMCS/src/sg/edu/nus/iss/vmcs/customer/controller/state/SuspendTransactionState.java
+++ WMCS/src/sg/edu/nus/iss/vmcs/customer/controller/state/SuspendTransactionState.java
@@ -1,5 +1,49 @@
 package sg.edu.nus.iss.vmcs.customer.controller.state;
 
-public class SuspendTransactionState {
+import sg.edu.nus.iss.vmcs.customer.controller.TransactionController;
 
+public class SuspendTransactionState implements TransactionState {
+
+	private TransactionController controller;
+	
+	public SuspendTransactionState(TransactionController controller) {
+		this.controller = controller;
+	}
+
+	@Override
+	public void startTransaction(int drinkIndex) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void processMoneyReceived(int total) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void completeTransaction() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void cancelTransaction() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void terminateFault() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void terminateTransaction() {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
